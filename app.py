9c9465ccdc99f1e04d251131af4d81c2a64e24f5
MarcoDiFrancesco <marcodifran@gmail.com>
2021-06-13 11:09:36 +0200

    Add normalization table

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ def main():
     PSW = "gigi"
     if password != PSW:
         return
+    # Make radio horizontal
+    st.write(
+        "<style>div.row-widget.stRadio > div{flex-direction:row;}</style>",
+        unsafe_allow_html=True,
+    )
     cal_type = st.radio("Group by", options=["Month", "Week", "Activity"])
 
     calendar_list = calendar.calendars["Calendar"].unique()
@@ -22,19 +27,16 @@ def main():
         df = calendar.by_week(calendar_sel, normalize=True)
     elif cal_type == "Activity":
         df = calendar.by_activity(calendar_sel)
-    show_table = st.checkbox("Show table")
 
-    normalize = st.checkbox("Normalize")
-    if normalize and not cal_type == "Activity":
-        df = df.div(df.sum(axis=1), axis=0)
-        # Sort columns by order of apperance
-        df = df[df.sum().sort_values().index[:]]
-        # Add number because chart orders by name
-        for i, row in enumerate(df):
-            df.rename(columns={row: f"{i}-{row}"}, inplace=True)
-
+    show_table = st.checkbox("Show data")
     if show_table:
         st.table(df)
+
+    # Sort data
+    if not cal_type == "Activity":
+        normalize_by_default = calendar_sel == "Select value"
+        df = sort_df(df, normalize_by_default)
+
     if cal_type == "Activity":
         alt_chart = (
             alt.Chart(df.reset_index())
@@ -49,6 +51,56 @@ def main():
     else:
         st.bar_chart(df, height=350)
         st.area_chart(df, height=350)
+
+
+def sort_df(df, normalize_by_default):
+    if normalize_by_default:
+        options = ["Standard deviation", "Sum", "Nope"]
+    else:
+        options = ["Nope", "Standard deviation", "Sum"]
+    normalize_by = st.radio("Normalize by", options=options)
+    if normalize_by == "Nope":
+        return df
+    if normalize_by == "Sum":
+        sorted_df = df.sum().sort_values()
+    elif normalize_by == "Standard deviation":
+        sorted_df = df.std().sort_values()
+
+    # Show table with std or sum
+    st.dataframe(sorted_df.sort_values(ascending=False).astype(int))
+
+    # Normalize by sum
+    df = df.div(df.sum(axis=1), axis=0)
+
+    # Sort one up, one down
+    new_sort = []
+    for element in sorted_df.index:
+        new_sort.insert(len(new_sort) // 2, element)
+
+    # Reverse order, in this case looks better
+    new_sort = new_sort[::-1]
+
+    # Reorder columns
+    df = df[new_sort]
+
+    # Add number because chart orders by name
+    for i, row in enumerate(df):
+        df.rename(columns={row: f"{i}-{row}"}, inplace=True)
+    return df
+
+
+# def sort_by_sum(df):
+#     sorted_df = df.sum().sort_values()
+#     st.dataframe(sorted_df)
+#     # Normalize by sum
+#     df = df.div(df.sum(axis=1), axis=0)
+#     df = df[sorted_df.index]
+#     return df
+
+
+# def sort_by_std(df):
+#     sorted_df = df.std().sort_values()
+#     st.dataframe(sorted_df)
 
 
 if __name__ == "__main__":
