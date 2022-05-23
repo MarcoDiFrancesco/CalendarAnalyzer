9a20bc3571c4bdd2483e19fec5d79d5fe9589220
MarcoDiFrancesco <marcodifran@gmail.com>
2022-05-23 09:12:07 +0200

    Fix spare time plot

--- utils/single_activity/spare_time.py
+++ utils/single_activity/spare_time.py
@@ -7,10 +7,8 @@ from utils.single_activity import chart_calendar_vert, chart_decreasing_activity
 
 def spare_time(df: pd.DataFrame) -> None:
     df = df.copy()
-    df = df[df["Calendar"] == "Work"]
-    if not len(df.index):
-        return
+    df = df[df["Calendar"] == "Spare time"]
     st.header("Spare time")
     df = remove_last_month(df, "DTSTART")
-    chart_calendar_vert(df, "Work")
-    chart_decreasing_activity(df, "Work")
+    chart_calendar_vert(df, "Spare time")
+    chart_decreasing_activity(df, "Spare time")
