a74f02af74ee366c9eeb7736911bac9d44ca8646
MarcoDiFrancesco <marcodifran@gmail.com>
2022-06-14 19:28:12 +0200

    Add beginning time of activities per week day

--- utils/all_activities.py
+++ utils/all_activities.py
@@ -122,31 +122,33 @@ def time_quality(df: pd.DataFrame) -> None:
 
 def beginning_of_day(df: pd.DataFrame) -> None:
     df = df.copy()
-    # Reindex because of duplicates given by merging muliple dataframes, one for
-    # each calendar
-    df = df.reset_index()
 
-    # Select only specific calendars
-    # df = df.loc[df["Calendar"].isin(["Eat", "Study"])]
+    # Reindex because of duplicates given by merging muliple dataframes, one for each calendar
+    df = df.reset_index()
 
     # Select column for debugging readability
     df = df[["Calendar", "SUMMARY", "DTSTART"]]
+
+    # Day name e.g. Monday
     df["DayName"] = df["DTSTART"].dt.day_name()
 
+    # Preserve original time
+    df["DTSTART_Orig"] = df["DTSTART"].dt.strftime("%Y/%m/%d %H:%M")
+
+    # Filter specific calendars
+    df = df.loc[df["Calendar"].isin(["Eat", "Study"])]
+
+    df = _filter_first_daily_activity(df)
+
     # Add randomly + or - 15 minutes to distribute activities during the half hour
-    # TODO: undestand why adding few seconds change completely the plot
-    arr_randmin = np.random.randint(0, 1, df.shape[0])
+    arr_randmin = np.random.randint(-14, 15, df.shape[0])
     df_randmin = pd.DataFrame({"int": arr_randmin})
-    df_randmin["time"] = pd.to_timedelta(df_randmin["int"], "sec")
+    df_randmin["time"] = pd.to_timedelta(df_randmin["int"], "min")
     df["DTSTART"] += df_randmin["time"]
 
     # Extract time from start date
-    df["Time"] = df["DTSTART"].dt.time
+    df["DTSTART_AltForm"] = df["DTSTART"].dt.strftime("%Y/%m/%d %H:%M")
 
-    # df = df.loc[df["Calendar"].isin(["Eat"])]
-
-    # TODO: undestand why half hours are not shown
-    # print(df)
     st.altair_chart(
         alt.Chart(df)
         .mark_circle(size=14)
@@ -158,17 +160,15 @@ def beginning_of_day(df: pd.DataFrame) -> None:
                 scale=alt.Scale(),
             ),
             y=alt.Y(
-                "hoursminutes(Time):T",
-                # scale=alt.Scale(domain=["2012-01-01T04:00:00", "2012-01-02T04:00:00"]),
-                scale=alt.Scale(domain=["2012-01-01T00:00:00", "2012-01-02T00:00:00"]),
+                "hoursminutes(DTSTART_AltForm):T",
+                scale=alt.Scale(domain=["2012-01-01T00:00:00", "2012-01-02T01:00:00"]),
             ),
             tooltip=[
                 alt.Tooltip("Calendar"),
                 alt.Tooltip("SUMMARY", title="Activity"),
-                # TODO: set to minutesseconds
-                alt.Tooltip("yearmonthdatehoursminutesseconds(Time)"),
+                alt.Tooltip("yearmonthdatehoursminutes(DTSTART_Orig)", title="Date"),
             ],
-            color=alt.Color("DayName:N", legend=None),
+            color=alt.Color("Calendar:N", legend=None),
             column=alt.Column(
                 "DayName:N",
                 header=alt.Header(
@@ -179,6 +179,15 @@ def beginning_of_day(df: pd.DataFrame) -> None:
                     labelAlign="center",
                     labelPadding=25,
                 ),
+                sort=[
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday",
+                    "Sunday",
+                ],
             ),
         )
         .transform_calculate(
@@ -190,3 +199,17 @@ def beginning_of_day(df: pd.DataFrame) -> None:
         .configure_axis(labelFontSize=16, titleFontSize=16)
         .properties(height=400, width=85)
     )
+
+
+def _filter_first_daily_activity(df: pd.DataFrame) -> pd.DataFrame:
+    """Get first activity per each day, for each calendar."""
+    df["DTSTART_tmp"] = df["DTSTART"]
+    df = df.set_index(["DTSTART_tmp", "Calendar"])
+    # Group by day and Calendar
+    df = df.groupby(
+        [pd.Grouper(level="DTSTART_tmp", freq="24h"), pd.Grouper(level="Calendar")]
+    ).first()
+    df = df.reset_index()
+    # Drop column that now contains dates like 2019-12-01
+    df = df.drop("DTSTART_tmp", axis=1)
+    return df
