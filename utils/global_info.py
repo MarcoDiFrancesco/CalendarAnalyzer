5ce79e1bad35ad93a47b498409e9cddee50c7ce1
MarcoDiFrancesco <marcodifran@gmail.com>
2022-06-16 17:22:10 +0200

    Change spacing in global info

--- utils/global_info.py
+++ utils/global_info.py
@@ -6,9 +6,12 @@ def global_info(df: pd.DataFrame) -> None:
     days = len(df["DAY"].unique())
     n_act = len(df)
     st.markdown(
+        # Trailing spaces removed by pre-commit
         f"""
         🟢 Beginning date: Dec 2019
+
         ⌛ Elapsed days: {days}
+
         🧮 Total activities: {n_act}
         """
     )
