326bea2df76578abfb89c5a029624f702dedf083
MarcoDiFrancesco <marcodifran@gmail.com>
2023-07-08 12:45:08 +0300

    Remove pylint from pre-commit and fix end date

--- utils/fill_month_values.py
+++ utils/fill_month_values.py
@@ -10,9 +10,8 @@ def fill_month_values(
     value_col,
 ) -> pd.DataFrame:
     # e.g. ["2019-12", "2020-01", ...]
-    months = pd.date_range("2019-12", datetime.datetime.today(), freq="MS").strftime(
-        "%Y-%m"
-    )
+    # months = pd.date_range("2019-12", end=datetime.datetime.today(), freq="MS").strftime("%Y-%m")
+    months = pd.date_range("2019-12", end="2022-12", freq="MS").strftime("%Y-%m")
     df_zeros = pd.DataFrame({month_col: months, value_col: np.zeros(len(months))})
     # Give priority to df, otherwise take df_zeros
     df_new = pd.concat([df, df_zeros])
